423069d4cd3e7f20e16d1750fb845cc092b6efd6
nabeelktr <nabee446@gmail.com>
2024-04-20 12:53:27 +0530

    cron job fn optimized

--- src/repository/notification.repository.ts
+++ src/repository/notification.repository.ts
@@ -2,9 +2,10 @@ import { DBConnectionError } from "@nabeelktr/error-handler";
 import { INotificationRepository } from "../interfaces/notification.Repository";
 import { Notification } from "../model/notification.entities";
 import NotificationModel from "../model/schemas/notificaiton.schema";
+import cron from "node-cron";
+import moment from "moment";
 
 export class NotificationRepository implements INotificationRepository {
-
   async getNotifications(data: any): Promise<Notification[] | null> {
     try {
       const notificaitons = await NotificationModel.find({
@@ -27,10 +28,22 @@ export class NotificationRepository implements INotificationRepository {
 
   async updateStatus(id: string): Promise<Object | null> {
     try {
-        const notification = await NotificationModel.findByIdAndUpdate(id, {status: "read"});
-        return { success: true };
-      } catch (e: any) {
-        throw new DBConnectionError();
-      }
+      const notification = await NotificationModel.findByIdAndUpdate(id, {
+        status: "read",
+      });
+      return { success: true };
+    } catch (e: any) {
+      throw new DBConnectionError();
+    }
   }
 }
+
+// delete notification using cron job
+cron.schedule("0 0 0 1 * *", async () => {
+  // trigger once a month at midnight on the 1st day of the month
+  const thirtyDaysAgo = moment().subtract(30, "days").toDate();
+  await NotificationModel.deleteMany({
+    status: "read",
+    createdAt: { $lt: thirtyDaysAgo },
+  });
+});
